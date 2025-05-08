9252cb07614411038e823b032db4a8797a18c8cd
Rudraksh Agarwal <rudraksh.agarwal@valtech.com>
2025-05-08 18:20:50 +0530

    schema-added

---
+++ backend/models/cars.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const carSchema = new mongoose.Schema({
+  Brand: {
+    type: String,
+    required: true
+  },
+  Model: {
+    type: String,
+    required: true
+  },
+  Year: {
+    type: Number,
+    required: true
+  },
+  FuelType: {
+    type: String,
+    enum: ['Petrol', 'Diesel', 'Hybrid', 'Electric'],
+    required: true
+  },
+  Price: {
+    type: Number,
+    required: true
+  },
+  CarType: {
+    type: String,
+    enum: ['Sedan', 'SUV', 'Coupe'],
+    required: true
+  },
+  Images: {
+    type: [String],
+    required: true
+  }
+});
+
+const Car = mongoose.model('Car', carSchema);
+
+module.exports = Car;
+
+
